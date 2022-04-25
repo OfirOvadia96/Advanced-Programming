14a8b0af31444efcabd20b2667401b81ce809ef0
Lioz Akirav <liozakirav@Liozs-MacBook-Pro.local>
2022-04-26 00:00:08 +0300

    upload task two

--- task2/stree.c
+++ task2/stree.c
@@ -1,35 +1,53 @@
-
 #define _XOPEN_SOURCE 600 /* Get nftw() */
 #include <ftw.h>
-#include <sys/types.h>    /* Type definitions used by many programs */
-#include <stdio.h>        /* Standard I/O functions */
-#include <stdlib.h>       /* Prototypes of commonly used library functions,
-                             plus EXIT_SUCCESS and EXIT_FAILURE constants */
-#include <unistd.h>       /* Prototypes for many system calls */
-#include <errno.h>        /* Declares errno and defines error constants */
-#include <string.h>       /* Commonly used string-handling functions */
-
+#include <sys/types.h> 
+#include <stdio.h>       
+#include <stdlib.h>      
+#include <unistd.h>     
+#include <errno.h>     
+#include <string.h>       
 #include <pwd.h>
 #include <grp.h>
+#include <limits.h>
+#include "stree.h"
 
-void mode_to_letters(int mode);
-void show_stat_info(const struct stat *buf);
+/* Convert permissions to letters */
+void convert_permissions(int per){
+
+char permissions_line[] = "---------";
+//owner permissions
+if ( per & S_IRUSR ) permissions_line[0] = 'r';
+if ( per & S_IWUSR ) permissions_line[1] = 'w'; 
+if ( per & S_IXUSR ) permissions_line[2] = 'x';
+//group permissions
+if ( per & S_IRGRP ) permissions_line[3] = 'r';
+if ( per & S_IWGRP ) permissions_line[4] = 'w';
+if ( per & S_IXGRP ) permissions_line[5] = 'x';
+//other users permissions
+if (per & S_IROTH ) permissions_line[6] = 'r';
+if (per & S_IWOTH ) permissions_line[7] = 'w';
+if (per & S_IXOTH ) permissions_line[8] = 'x';
+
+
+printf("%s" , permissions_line); //Print the built permessions line
+}
 
 /* Callback function called by ftw() */
-static int dirTree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb){
+static int dir_my_tree(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb){
 
     if (type != FTW_NS){
-        //printf("%7ld ", (long) sbuf->st_ino);
-        }
+
+    }
     else {printf("        ");}
 	
-        if (type == FTW_NS) {                  /* Could not stat() file */
+    if (type == FTW_NS) {               
         printf("?");
-        }
-     else {
-    	 printf(" %*s", 4 * ftwb->level, "|__ ");         /* Indent suitably */
-    	    
-        switch (sbuf->st_mode & S_IFMT) {  /* Print file type */
+    }
+
+    else {
+    	 printf(" %*s", 4 * ftwb->level, "|__ ");       
+    	/* Print file type */
+        switch (sbuf->st_mode & S_IFMT) {  
         case S_IFREG:  printf("[-"); break;
         case S_IFDIR:  printf("[d"); break;
         case S_IFCHR:  printf("[c"); break;
@@ -37,71 +55,53 @@ static int dirTree(const char *pathname, const struct stat *sbuf, int type, stru
         case S_IFLNK:  printf("[l"); break;
         case S_IFIFO:  printf("[p"); break;
         case S_IFSOCK: printf("[s"); break;
-        default:       printf("[?"); break; /* Should never happen (on Linux) */
+        default:       printf("[?"); break; //never happend (on Linux)
         }
         
     }
-    show_stat_info(sbuf);
-    printf("] ");
-        printf("%s\n",  &pathname[ftwb->base]);     /* Print basename */
-
-    return 0;                                   /* Tell nftw() to continue */
     
+    convert_permissions(sbuf->st_mode);
+	printf(" %s", getpwuid(sbuf->st_uid)->pw_name); 
+	printf(" %s", getgrgid(sbuf->st_gid)->gr_name); 
+	printf(" %ld", sbuf->st_size);
+    printf("] ");
+        printf("%s\n",  &pathname[ftwb->base]);    
 
+    return 0;
 }
 
 int main(int argc, char *argv[]){
-
-    if (argc == 1) {
-        char* str = argv[0];
-        printf("ERROR! no input: %s need argument\n", &str[2]);
-        exit(1);
-    }
 
     if(argc > 2){
         printf("ERROR! too many agruments\n");
         exit(1);
     }
 
-    if (nftw(argv[1], dirTree, 10, 0) == -1) {
-        printf("args not valid: yes its -1 \n");
-        perror("nftw");
-        exit(1);
+    /* no Arguments at all*/
+    else if (argc == 1) {
+        
+        /* find current path */
+        char cwd[PATH_MAX];
+        if(getcwd(cwd, sizeof(cwd)) == NULL){
+            printf("ERRO!, can't find our path");
+            exit(1);
+        }
+
+        /* print tree */
+        if (nftw(cwd, dir_my_tree, 10, 0) == -1) {
+            perror("args not valid");
+            exit(1);
+        }
+    }
+
+    /* One Argument (need to be valid path) */
+    else if(argc == 2){
+        if (nftw(argv[1], dir_my_tree, 10, 0) == -1) {
+            perror("args not valid");
+            exit(1);
+        }
     }
 
     return 0;
 }
-
-
-
-void show_stat_info(const struct stat *buf){
-
-	mode_to_letters(buf->st_mode);
-	printf(" %s", getpwuid(buf->st_uid)->pw_name); 
-	printf(" %s", getgrgid(buf->st_gid)->gr_name); 
-	printf(" %ld", buf->st_size); 
-
-}
-
-void mode_to_letters(int mode){
-
-char str[] = "---------";
-
-if ( mode & S_IRUSR ) str[0] = 'r'; /* 3 bits for user */
-if ( mode & S_IWUSR ) str[1] = 'w';
-if ( mode & S_IXUSR ) str[2] = 'x';
-
-if ( mode & S_IRGRP ) str[3] = 'r'; /* 3 bits for group */
-if ( mode & S_IWGRP ) str[4] = 'w';
-if ( mode & S_IXGRP ) str[5] = 'x';
-
-if (mode & S_IROTH ) str[6] = 'r'; /* 3 bits for other */
-if (mode & S_IWOTH ) str[7] = 'w';
-if (mode & S_IXOTH ) str[8] = 'x';
-
-printf("%s",str);
-}
-
-
-	
 
